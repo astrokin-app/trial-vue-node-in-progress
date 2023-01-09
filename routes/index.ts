dd66e3c49463543c0e7f602acfb6a6f0419e140c
astrokre <kre.astrokin@gmail.com>
2023-01-09 19:44:55 +0100

    remove doublon folder routes

--- routes/index.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { defaultRoute } from './defaultRoute';
-import { usersRoute } from './users/route';
-
-export const routes = express.Router();
-
-routes.use(defaultRoute, usersRoute);
