dafe41772b4e3ae313755d7a92c60355c473bd2c
astrokre <kre.astrokin@gmail.com>
2023-01-13 16:36:54 +0100

    ts++

--- server/controllers/usersController.ts
+++ server/controllers/usersController.ts
@@ -1,16 +1,17 @@
 import { Request, Response } from 'express'
 import { QueryResult } from 'pg'
 import { pool } from '../config/dbConfig'
+import { UserModel } from "../models/users/model";
 import { signAccessToken } from '../utils/jwt'
 require("dotenv").config()
 
-// TODO bcrypt password && model user
+// TODO bcrypt password + double confirmation
 export const createUser = async (req: Request, res: Response): Promise<Response> => {
   try {
     const {username, email, password} = req.body
-    await pool.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3)', [username, email, password]) as QueryResult
+    await pool.query<UserModel>('INSERT INTO users (username, email, password) VALUES ($1, $2, $3)', [username, email, password]) as QueryResult<UserModel>
 
-    return res.status(200).json({ access_token: signAccessToken(email, password) })
+    return res.status(200).json({ access_token: signAccessToken(email, username)})
   } catch (e) {
     console.error(e)
     return res.status(401).json({ message: e.toString() })
@@ -20,9 +21,10 @@ export const createUser = async (req: Request, res: Response): Promise<Response>
 export const getUserByEmailPassword = async (req: Request, res: Response): Promise<Response> => {
   try {
     const {email, password} = req.body
-    await pool.query('SELECT * FROM users WHERE email = $1 and password = $2', [email, password]) as QueryResult
+    const user = await pool.query<UserModel>('SELECT * FROM users WHERE email = $1 and password = $2', [email, password]) as QueryResult<UserModel>
+    console.log(user)
 
-    return res.status(200).json({ access_token: signAccessToken(email, password) })
+    return res.status(200).json({ access_token: signAccessToken(user.rows[0].email, user.rows[0].username)})
   } catch (e) {
     console.error(e)
     return res.status(401).json({ message: e.toString() })
