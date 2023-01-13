40863028b420378e1742e9d61a1ff6ed72945bfe
astrokre <kre.astrokin@gmail.com>
2023-01-13 12:31:31 +0100

    clean code+

--- server/controllers/usersController.ts
+++ server/controllers/usersController.ts
@@ -8,40 +8,23 @@ require("dotenv").config()
 export const createUser = async (req: Request, res: Response): Promise<Response> => {
   try {
     const {username, email, password} = req.body
-    const response: QueryResult = await pool.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3)', [username, email, password])
-
-    if (!response.rowCount) {
-      return res.status(401).json({ message: "error_createUser" })
-    }
+    await pool.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3)', [username, email, password]) as QueryResult
 
     return res.status(200).json({ access_token: signAccessToken(email, password) })
-
   } catch (e) {
-    console.log(e)
-    throw new Error(`error_createUser: ${e.toString()}`);
-
-  } finally {
-    return res.status(401).json({ message: "error_createUser" })
+    console.error(e)
+    return res.status(401).json({ message: e.toString() })
   }
 }
 
 export const getUserByEmailPassword = async (req: Request, res: Response): Promise<Response> => {
   try {
     const {email, password} = req.body
-    const response: QueryResult = await pool.query('SELECT * FROM users WHERE email = $1 and password = $2', [email, password])
-    console.log('ENDPOINT LOGIN', response)
-
-    if (!response.rows[0]) {
-      return res.status(401).json({ message: "error_getUserByEmailPassword" })
-    }
+    await pool.query('SELECT * FROM users WHERE email = $1 and password = $2', [email, password]) as QueryResult
 
     return res.status(200).json({ access_token: signAccessToken(email, password) })
-
   } catch (e) {
-    console.log(e)
-    throw new Error(`error_getUserByEmailPassword: ${e.toString()}`);
-
-  } finally {
-    return res.status(401).json({ message: "error_getUserByEmailPassword" })
+    console.error(e)
+    return res.status(401).json({ message: e.toString() })
   }
 }
