28db9e080217cc0d532eb049cba11d90c2163b64
astrokre <kre.astrokin@gmail.com>
2024-07-02 16:09:29 +0200

    remove console log

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -9,14 +9,12 @@ require("dotenv").config({ path: '../../.env' })
 // TODO bcrypt password + double confirmation
 export const createUser = async (req: Request, res: Response): Promise<Response> => {
   try {
-    console.log('yooo')
     const {username, email, password} = req.body
     const hashedPassword = await hashPassword(password) as { hash: string, salt: string}
-    console.log(hashedPassword)
     const hashPwd = hashedPassword.hash
     const saltPwd = hashedPassword.salt
+
     await pool.query<UserModel>('INSERT INTO users (username, email, password) VALUES ($1, $2, $3)', [username, email, hashPwd + '|' + saltPwd]) as QueryResult<UserModel>
-    console.log(res)
     return res.status(200).json({ access_token: signAccessToken(email, username)})
   } catch (e) {
     return res.status(401).json({ message: e.toString() })
